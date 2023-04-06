10dcd7e7369b0258e19c4ab7c0378a2bd46573b5
XenoBIT78 <xenobit78@gmail.com>
2023-04-06 19:34:02 +0200

    Update sudo.sh

--- sudo.sh
+++ sudo.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-su 
+
+# Check Root Privledges
+if [[ $EUID -ne 0 ]]; then
+  echo -e "$0 requires root privledges"
+  echo -e "use su - to switch to root account"
+  exit 1
+fi
+
+
+# Install sudo
+apt install sudo -y
+
+# Link usermod to /usr/bin
 ln /usr/sbin/usermod /usr/bin
-usermod -aG sudo 
+
+#add user to sudo grp
+read -p "Enter user to add to sudo grp.\n" user
+usermod -aG sudo $user
+user $user is added to sudo grp.
+
+id $user
+
+exit 0
