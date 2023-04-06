1fa565d44696ab8c4462432bb628322ad5cc5d83
XenoBIT78 <xenobit78@gmail.com>
2023-04-06 19:42:11 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Check Root Privledges
+if [[ $EUID -ne 0 ]]; then
+  echo -e "$0 requires root privledges"
+  echo -e "use su - to switch to root account"
+  exit 1
+fi
+
 # Requirements
 if command -v zsh &> /dev/null && command -v git &> /dev/null && command -v wget &> /dev/null; then
     echo -e "ZSH and Git are already installed\n"
