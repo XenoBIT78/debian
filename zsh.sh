c0e52c07caf01282b27c890e2019b064cf2d507b
XenoBIT78 <xenobit78@gmail.com>
2023-04-06 19:41:30 +0200

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-# Install zsh
-apt install zsh
+# Requirements
+if command -v zsh &> /dev/null && command -v git &> /dev/null && command -v wget &> /dev/null; then
+    echo -e "ZSH and Git are already installed\n"
+else
+    if sudo apt install -y zsh git wget ; then
+        echo -e "zsh wget and git Installed\n"
+    else
+        echo -e "Please install the following packages first, then try again: zsh git wget \n" && exit
+    fi
+fi
